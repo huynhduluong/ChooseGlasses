3bc603972fb5adef4be41dd7ba80d581d7be89b7
Huynh Du Luong <huynhduluong@gmail.com>
2020-10-29 20:10:31 +0700

    Updates

---
+++ precache-manifest.c4b7aa60eb16fe777cb0211c60c5386d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a668f28cc9df23496faa9fba18be3e9",
+    "url": "/ChooseGlasses/index.html"
+  },
+  {
+    "revision": "8a243dc0f80cb899cb55",
+    "url": "/ChooseGlasses/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e1b08940a86ffd2f37ce",
+    "url": "/ChooseGlasses/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a243dc0f80cb899cb55",
+    "url": "/ChooseGlasses/static/js/2.be42367d.chunk.js"
+  },
+  {
+    "revision": "48708d0bba57d355d4ffc14a8bfac5cf",
+    "url": "/ChooseGlasses/static/js/2.be42367d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b08940a86ffd2f37ce",
+    "url": "/ChooseGlasses/static/js/main.7b1f97e5.chunk.js"
+  },
+  {
+    "revision": "cbef52f8652645ac4219",
+    "url": "/ChooseGlasses/static/js/runtime-main.69fe6155.js"
+  }
+]);
